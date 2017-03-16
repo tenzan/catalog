f008958e5c14c92b09b83f316454c46c88d31c4a
Askarbek Karasasev <askar.karasaev@gmail.com>
2017-03-16 22:07:17 +0900

    Adding images

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,13 @@ class Item < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   validates :name, :image, presence: true
+
+  validates_processing_of :image
+  validate :image_size_validation
+
+  private
+  def image_size_validation
+    errors[:image] << "should be less than 2500KB" if image.size > 2.5.megabytes
+  end
+
 end
